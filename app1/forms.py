5375da85f6ad0c27e26609c47c678377eea2e82d
Deepanshu Taneja <deepanshutaneja762@gmail.com>
2024-03-24 23:36:26 +0530

    added signup email

--- app1/forms.py
+++ app1/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import Contact
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 
 class ContactForm(forms.ModelForm):
@@ -9,5 +12,20 @@ class ContactForm(forms.ModelForm):
         fields = [
             "email","subject","message",
         ]
+
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(label='Email')  # Define the email field directly in the class
+
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2", "email")  # Include email field in Meta
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
+        self.fields['username'].help_text = None
 
 
