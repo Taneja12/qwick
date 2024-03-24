b98c84e859ae90f5e45eb4de8ba1d5e72df6b357
Deepanshu Taneja <deepanshutaneja762@gmail.com>
2024-03-24 17:15:06 +0530

    Added quantity functionality

--- Qwicky/settings.py
+++ Qwicky/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Qwicky.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'app1/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
